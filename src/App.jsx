eec12daf8be09400105be92c457f98c46ac5dd3d
sabrinapupo21 <sabrinamaiapupo@hotmail.com>
2026-04-02 15:41:49 -0300

    Ajustada responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Custos de Registro: Compra, Venda e Financiamento (Tabela SP)</h1>
-        <Calculadora />
+        <div className="App-wrapper">
+          <h1>
+            Custos de Registro: Compra, Venda e Financiamento <br />
+            (Tabela SP)
+          </h1>{" "}
+          <Calculadora />
+        </div>
       </div>
     </>
   );
